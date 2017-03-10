854491a609c73cd7727cfe2d3a41d21b6a5fa11a
stuartervine <stuart.ervine@shift-f6.com>
2017-03-10 18:36:49 +0000

    Simplifying the stacker example based off feedback to make it easier to see where to put the predicates.

--- src/test/java/com/shiftf6/workshop1/functionaljava/Stacker.java
+++ src/test/java/com/shiftf6/workshop1/functionaljava/Stacker.java
@@ -1,5 +1,6 @@
 package com.shiftf6.workshop1.functionaljava;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Stacker {
@@ -14,22 +15,30 @@ public class Stacker {
 
     public int stackableItemsArea(List<Shape> shapes) {
         int area = 0;
+        List<Shape> stackableShapes = new ArrayList<>();
         for (Shape shape : shapes) {
             if (shape.stackable) {
                 if (shape.width * shape.height <= 15) {
-                    area += shape.width * shape.height;
+                    stackableShapes.add(shape);
                 }
             }
+        }
+        for (Shape shape : stackableShapes) {
+            area += shape.width * shape.height;
         }
         return area;
     }
 
     public int unstackableItemsArea(List<Shape> shapes) {
         int area = 0;
+        List<Shape> unstackableShapes = new ArrayList<>();
         for (Shape shape : shapes) {
-            if(!shape.stackable || shape.width * shape.height > 15) {
-                    area += shape.width * shape.height;
+            if (!shape.stackable || shape.width * shape.height > 15) {
+                unstackableShapes.add(shape);
             }
+        }
+        for (Shape shape : unstackableShapes) {
+            area += shape.width * shape.height;
         }
         return area;
     }
